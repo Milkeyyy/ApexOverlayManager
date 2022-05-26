6ec0ac46f02eb407a784cf68a37cdc7f59785364
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2022-05-26 11:52:29 +0900

    Version 0.1.0
    OBSとの接続とOCRを全てプロデルでの処理に統合、その他一通りの機能実装

--- Source/main.py
+++ Source/main.py
@@ -2,6 +2,7 @@ import argparse
 from re import sub
 import subprocess
 import time
+import json
 
 #import pyocr
 from obswebsocket import obsws, requests
@@ -10,7 +11,7 @@ name = "ApexOverlayManager"
 version = "0.1.0"
 author = "Milkeyyy"
 
-print(f"{name} Version {version}\nCreated by {author}")
+print(f"\n{name} Version {version}\nCreated by {author}\n")
 
 #引数ぱーさー
 parser = argparse.ArgumentParser()
@@ -33,6 +34,11 @@ scene = args.scene
 source = args.source
 loopv = True
 
+#コンフィグを読み込み
+config_open = open("Config.json", "r", encoding="UTF-8")
+config = json.load(config_open)
+print(f"コンフィグを読み込みました: {config}\n")
+
 def switch_overlay(render):
 	response = obs.call(requests.SetSceneItemRender(source,render,scene))
 	print(f"	{response}")
@@ -48,18 +54,35 @@ def disable_overlay():
 #OBSに接続する
 obs = obsws(host,port,password)
 obs.connect()
-print(f"Connect: {host}:{port}")
+print(f"OBS WebSocketと接続: {host}:{port}\n-準備完了-")
 
 while loopv == True:
-	ocr_r = subprocess.run([r"OCRScreenCaptureHelper.exe"], shell=True, text=True, stdout=subprocess.PIPE)
-	if ocr_r.stdout == "True":
-		print(f"オーバーレイ有効化 ({ocr_r.stdout})")
-		switch_overlay(True)
-	else:
-		print(f"オーバーレイ無効化 ({ocr_r.stdout})")
-		switch_overlay(False)
+	#画面をキャプチャー
+	ocr_c = subprocess.run([r"OCRScreenCaptureHelper.exe"], shell=True, text=True, stdout=subprocess.PIPE)
+	#print(f"キャプチャー画像: {ocr_c.stdout}")
 
-	#time.sleep(1)
+	#キャプチャーした画像から文字認識
+	ocr_w = subprocess.run([r"OCRWorker.exe", ocr_c.stdout], shell=True, text=True, stdout=subprocess.PIPE)
+	ocr_w_text = ocr_w.stdout.rstrip("\n")
+
+	detect = False
+	sourcerender = obs.call(requests.GetSceneItemProperties(source,scene)).datain["visible"]
+	for w in config["ListOfWordToDetect"]:
+		if w in ocr_w_text:
+			detect = True
+			break
+
+	#単語を検知できたらオーバーレイを有効化
+	if detect == True:
+		if sourcerender is False:
+			print(f"\nオーバーレイ有効化 | 検知文字: {ocr_w_text}")
+			switch_overlay(True)
+	else:
+		if sourcerender is True:
+			print(f"\nオーバーレイ無効化 | 検知文字: {ocr_w_text}")
+			switch_overlay(False)
+
+	#time.sleep(0.1)
 
 #オーバーレイを有効化/無効化
 #print(f"Render: {args.render}")
--- _Compile-GUI.bat
+++ _Compile-GUI.bat
@@ -3,3 +3,4 @@ cd %~dp0
 echo コンパイルを開始
 echo.
 "C:\Users\Milkeyyy\AppData\Local\Programs\Produire\rdrc.exe" ApexOverlayManager.rdrproj
+pause
