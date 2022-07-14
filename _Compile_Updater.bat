7858e69bada80921d2cf508a55b56a1fcefbc382
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2022-07-14 17:02:17 +0900

    gitignore更新

--- _Compile_Updater.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd %~dp0
-echo コンパイルを開始
-echo.
-"C:\Users\Milkeyyy\AppData\Local\Programs\Produire\rdrc.exe" AOM_Updater.rdrproj
-rem "C:\Users\Milkeyyy\Apps\produire-1.8\rdrc.exe" AOM_Updater.rdrproj
